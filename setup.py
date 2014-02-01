210c416d93d921574bf68f2446ea926351c9379d
Dan Faulknor <daniel@zzrv.co>
2014-02-01 20:15:29 +1100

    Centralisaion

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python
 from lib import state
-state.StateTable.create_table()
+from lib.config import config
+import socket
+import requests
+import sys
+import os
+import json
+from git import Repo
+#state.StateTable.create_table()
 
+root = config.get("general", "path")
+
+payload = {'format': 'json', 'server__name': socket.gethostname()}
+r = json.loads(requests.get("http://" + config.get("general", "confighost") + "/api/v1/module/", params=payload).text)
+
+modules = {}
+for module in r['objects']:
+	payload = {'format': 'json', 'name': module['name']}
+	moduleData = json.loads(requests.get("http://" + config.get("general", "confighost") + "/api/v1/module_list/", params=payload).text)['objects'][0]
+        modules[module['id']] =  { "name" : module['name'], "package" : moduleData["package"], "config" : moduleData["config"] };
+modules_list = ""
+for moduleID, moduleInfo in modules.iteritems():
+#	Repo.clone_from(moduleInfo['package'], "modules/" + str(moduleInfo['name']))
+	module = str(moduleInfo['name'])
+	if moduleInfo['config']:
+		configSplit = moduleInfo['config'].split("=")
+		try:
+			config.add_section(module)
+		except:
+			pass
+		config.set(module, str(configSplit[0]).strip(), configSplit[1])
+	target = open ("modules/" + module + "/id.txt", 'w')
+	target.write(str(moduleID))
+	target.close
+        if os.path.isfile(os.path.join(root,module,"setup.py")):
+		execfile(os.path.join(root,module,"setup.py"))
+	
+with open (os.path.dirname(os.path.realpath(__file__)) + '/config.ini', 'wb') as configfile:
+	config.write(configfile)
